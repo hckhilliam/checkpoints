0b1dab74e6f97bcfab8108283de1380507dcc58c
Ernest Ho <ernestho.ho@gmail.com>
2016-10-23 03:47:35 -0400

    Ink ripplesgit add .

--- web/components/InkRipple.tsx
+++ web/components/InkRipple.tsx
@@ -6,11 +6,12 @@ import * as update from 'immutability-helper';
 import * as ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 interface InkRippleProps extends React.HTMLAttributes {
-  duration?: number;
+
 }
 
 interface InkRippleState {
   ripples?: Ripple[];
+  style?: React.CSSProperties;
 }
 
 interface Ripple {
@@ -19,35 +20,48 @@ interface Ripple {
   y: number;
 }
 
-const RippleElement = (props: { ripple: Ripple }) => {
+const RippleElement = ({ ripple }: { ripple: Ripple }) => {
+  const style = {
+    top: ripple.y,
+    left: ripple.x
+  };
   return (
-    <div className="RippleElement" />
+    <div className="RippleElement" style={style} />
   )
 }
 
+const enterDuration = 300;
+const leaveDuration = 200;
+
 export default class InkRipple extends React.Component<InkRippleProps, InkRippleState> {
   static defaultProps: InkRippleProps = {
-    duration: 2000
+
   }
 
   state: InkRippleState = {
-    ripples: []
+    ripples: [],
+    style: {
+      width: '100%',
+      height: '100%',
+      position: 'relative'
+    }
   }
 
+  node: HTMLDivElement;
   counter = 0;
 
-  startRipple() {
+  startRipple(x: number, y: number) {
     const counter = ++this.counter;
     window.setTimeout(() => {
       this.setState({
         ripples: this.state.ripples.filter(r => r.id != counter)
       });
-    }, this.props.duration);
+    }, enterDuration);
 
     const ripple: Ripple = {
       id: counter,
-      x: 0,
-      y: 0
+      x,
+      y
     };
     const ripples = update(this.state.ripples, {
       $push: [ripple]
@@ -56,19 +70,62 @@ export default class InkRipple extends React.Component<InkRippleProps, InkRipple
   }
 
   handleRipple = (event: React.MouseEvent) => {
-    console.log(event.clientX, event.clientY);
-    this.startRipple();
+    let x = 0, y = 0;
+    if (this.node) {
+      const rect = this.node.getBoundingClientRect();
+      const { top, left } = rect;
+      x = event.clientX - left;
+      y = event.clientY - top;
+    }
+    this.startRipple(x, y);
+  }
+
+  handleRef = (node: HTMLDivElement) => {
+    this.node = node;
+    const size = Math.max(node.clientWidth, node.clientHeight);
+
+    let left: number | string;
+    let top: number | string;
+    let transform = '';
+    if (node.clientWidth > node.clientHeight) {
+      left = 0;
+      top = '50%';
+      transform = 'translateY(-50%)';
+    } else {
+      left = '50%';
+      top = 0;
+      transform = 'translateX(-50%)';
+    }
+
+    this.setState({
+      style: {
+        position: 'relative',
+        width: size,
+        height: size,
+        left,
+        top,
+        transform
+      }
+    });
   }
 
   render() {
     const { className } = this.props;
-    const other = _.omit(this.props, 'className', 'duration');
+    const other = _.omit(this.props, 'className');
 
     const cssClass = classnames('InkRipple', className);
 
     return (
       <div className={cssClass} {...other} onMouseDown={this.handleRipple}>
-        {this.state.ripples.map(r => <RippleElement key={r.id} ripple={r} />)}
+        <div className="test" style={this.state.style} ref={this.handleRef}>
+          <ReactCSSTransitionGroup
+            transitionName="ripple"
+            transitionEnterTimeout={enterDuration}
+            transitionLeaveTimeout={leaveDuration}
+          >
+            {this.state.ripples.map(r => <RippleElement key={r.id} ripple={r} />)}
+          </ReactCSSTransitionGroup>
+        </div>
       </div>
     );
   }
