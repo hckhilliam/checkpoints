7da2d9d7622eb2225c819a550c65feb272c6d4d3
Ernest Ho <ernestho.ho@gmail.com>
2016-10-26 06:20:56 -0400

    Add /me/info endpoint

--- server/routes/me.ts
+++ server/routes/me.ts
@@ -4,6 +4,14 @@ import { Router, Request, Response } from 'express';
 
 const api = Router();
 
+api.get('/info', (req: Request, res: Response) => {
+  const { _id, name } = req['user'];
+  res.json({
+    id: _id,
+    name
+  });
+});
+
 api.get('/hello', (req: Request, res: Response) => {
   res.send(`Hey ${req['user'].name} ${req['user']._id}!`);
 })
