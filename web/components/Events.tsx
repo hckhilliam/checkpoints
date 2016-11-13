1427d8dc04ceca42ef4cd764d0fce58620ea1534
jsnlm <jasonjslam@gmail.com>
2016-11-13 00:38:57 -0500

    added styles to the events

--- web/components/Events.tsx
+++ web/components/Events.tsx
@@ -4,15 +4,29 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { getEvents } from '../actions/events';
 import Event from './Event';
+import { List, ExpandableListItem } from './List';
 
 interface Props {
   events?: Checkpoints.Event[];
   onGetEvents?: (eventSearch) => void;
 }
 
-export class Events extends React.Component<Props, {}> {
+interface State {
+  selectEventID?: string;
+}
+
+export class Events extends React.Component<Props, State> {
   componentDidMount() {
     this.props.onGetEvents(this.getSearchQuery());
+  }
+
+  state: State = {
+    selectEventID: ''
+  }
+
+  // id is just a concat of an event's name, description and eventSource
+  getSelectEventID(event: Checkpoints.Event) {
+    return event.name + event.description + event.eventSource;
   }
 
   getSearchQuery = () => {
@@ -21,18 +35,39 @@ export class Events extends React.Component<Props, {}> {
       lng: -73.964040,
       distance: undefined,
       filter: undefined
-    } as Checkpoints.eventSearch; 
+    } as Checkpoints.eventSearch;
+  }
+
+  onClickEvent(id: string) {
+    let prevSelected = this.state.selectEventID;
+    if (prevSelected == id) {
+      this.setState({ selectEventID: "" });
+    }
+    else {
+      this.setState({ selectEventID: id });
+    }
   }
 
   render() {
     return (
-      <div className="Events">
+      <List className="Events">
       {
-        this.props.events.map((item, index) => {
-          return <Event event={item} key={index}/>
+        this.props.events.map((event) => {
+          let id = this.getSelectEventID(event);
+          const selected = id == this.state.selectEventID;
+          return (
+            <ExpandableListItem
+              selected={selected}
+              key={id}
+              loading={false}
+              onClick={() => this.onClickEvent(id)}
+            >
+              <Event event={event} />
+            </ExpandableListItem>
+          )
         })
       }
-      </div>
+      </List>
     );
   };
 }
