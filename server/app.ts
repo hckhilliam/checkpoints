51db4c2c63b293d531258afc0f40e6edca4702a4
William Ma <williamjma@hotmail.com>
2016-11-20 21:21:47 -0500

    added favicon and fixed overflow on searchbar

--- server/app.ts
+++ server/app.ts
@@ -12,8 +12,6 @@ import { job } from './jobs/scheduler';
 
 const app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
