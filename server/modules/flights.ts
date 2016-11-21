ed50e26010900423893abd078b6b515081690268
jsnlm <jasonjslam@gmail.com>
2016-11-21 01:06:53 -0500

    fixes for flight recommendations

--- server/modules/flights.ts
+++ server/modules/flights.ts
@@ -119,9 +119,9 @@ export function cleansePlaces(places: Skyscanner.Place[]) {
 export function searchUserFlights(user: CheckpointsServer.User): Promise<CheckpointsServer.Flight> {
   return getActiveCheckpoints(user._id).then(ret => {
     let checkpoints = ret as any as CheckpointsServer.Checkpoint[];
-    let titlesConcated = checkpoints.map(checkpoint => checkpoint.title).join(" ");
+    let titlesConcated = checkpoints.map(checkpoint => checkpoint.title).join("  ").toLowerCase();
 
-    let destinations = Array.from(searchablePlaces).filter(destination => titlesConcated.includes(destination));
+    let destinations = Array.from(searchablePlaces).filter(destination => titlesConcated.includes(destination.toLowerCase()));
     
     return Promise.all(destinations.map(destination => {
       return autoSuggest(destination).then(places => {
@@ -150,35 +150,42 @@ export function searchUserFlights(user: CheckpointsServer.User): Promise<Checkpo
 }
 
 const places = [
-  'United States',
-  'China',
-  'Spain',
-  'France',
-  'United Kingdom',
-  'Thailand',
-  'Italy',
-  'Germany',
-  'Hong Kong',
-  'Macau',
-  'Australia',
-  'Turkey',
-  'Shanghai',
-  'Pattaya',
-  'Miami',
-  'Phuket',
-  'Guangzhou',
-  'Taipei',
-  'Rome',
-  'Seoul',
-  'Dubai',
-  'Antalya',
-  'Kuala Lumpur',
-  'Istanbul',
-  'New York',
-  'Shenzhen',
-  'Paris',
-  'Bangkok',
-  'Singapore',
-  'London',
+  //http://brilliantmaps.com/top-100-tourist-destinations/
+  'Hong Kong', 'Singapore', 'Bangkok', 'London', 'Macau', 'Kuala Lumpur', 'Shenzhen', 'New York City', 'Antalya', 'Paris', 'Istanbul', 'Rome', 'Dubai', 
+'Guangzhou', 'Phuket', 'Mecca', 'Pattaya', 'Taipei City', 'Prague', 'Shanghai', 'Las Vegas', 'Miami', 'Barcelona', 'Moscow', 'Beijing', 'Los Angeles', 
+'Budapest', 'Vienna', 'Amsterdam', 'Sofia', 'Madrid', 'Orlando', 'Ho Chi Minh City', 'Lima', 'Berlin', 'Tokyo', 'Warsaw', 'Chennai', 'Cairo', 'Nairobi', 
+'Hangzhou', 'Milan', 'San Francisco', 'Buenos Aires', 'Venice', 'Mexico City', 'Dublin', 'Seoul', 'Mugla', 'Mumbai', 'Denpasar', 'Delhi', 'Toronto', 
+'Zhuhai', 'St Petersburg', 'Burgas', 'Sydney', 'Djerba', 'Munich', 'Johannesburg', 'Cancun', 'Edirne', 'Suzhou', 'Bucharest', 'Punta Cana', 'Agra', 
+'Jaipur', 'Brussels', 'Nice', 'Chiang Mai', 'Sharm el-Sheikh', 'Lisbon', 'East Province', 'Marrakech', 'Jakarta', 'Manama', 'Hanoi', 'Honolulu', 
+'Manila', 'Guilin', 'Auckland', 'Siem Reap', 'Sousse', 'Amman', 'Vancouver', 'Abu Dhabi', 'Kiev', 'Doha', 'Florence', 'Rio de Janeiro', 'Melbourne', 
+'Washington D.C.', 'Riyadh', 'Christchurch', 'Frankfurt', 'Baku', 'Sao Paulo', 'Harare', 'Kolkata', 'Nanjing', 'Hong Kong', 'Singapore', 'Thailand', 
+'UK', 'Macau', 'Malaysia', 'China', 'USA', 'Turkey', 'France', 'Turkey', 'Italy', 'UAE', 'China', 'Thailand', 'Saudi Arabia', 'Thailand', 'Taiwan', 
+'Czech Republic', 'China', 'USA', 'USA', 'Spain', 'Russia', 'China', 'USA', 'Hungary', 'Austria', 'Netherlands', 'Bulgaria', 'Spain', 'USA', 'Vietnam', 
+'Peru', 'Germany', 'Japan', 'Poland', 'India', 'Egypt', 'Kenya', 'China', 'Italy', 'USA', 'Argentina', 'Italy', 'Mexico', 'Ireland', 'South Korea', 
+'Turkey', 'India', 'Indonesia', 'India', 'Canada', 'China', 'Russia', 'Bulgaria', 'Australia', 'Tunisia', 'Germany', 'South Africa', 'Mexico', 'Turkey', 
+'China', 'Romania', 'Dominican Republic', 'India', 'India', 'Belgium', 'France', 'Thailand', 'Egypt', 'Portugal', 'Saudi Arabia', 'Morocco', 'Indonesia', 
+'Bahrain', 'Vietnam', 'USA', 'Philippines', 'China', 'New Zealand', 'Cambodia', 'Tunisia', 'Jordan', 'Canada', 'UAE', 'Ukraine', 'Qatar', 'Italy', 
+'Brazil', 'Australia', 'USA', 'Saudi Arabia', 'New Zealand', 'Germany', 'Azerbaijan', 'Brazil', 'Zimbabwe', 'India', 'China',
+
+//http://www.citymayors.com/statistics/largest-cities-population-125.html
+'Tokyo', 'New York Metro', 'Sao Paulo', 'Seoul', 'Mexico City', 'Osaka', 'Manila', 'Mumbai', 'Delhi', 'Jakarta', 'Lagos', 'Kolkata', 'Cairo', 'Los Angeles', 
+'Buenos Aires', 'Rio de Janeiro', 'Moscow', 'Shanghai', 'Karachi', 'Paris', 'Istanbul', 'Nagoya', 'Beijing', 'Chicago', 'London', 'Shenzhen', 'Essen', 'Tehran', 
+'Bogota', 'Lima', 'Bangkok', 'Johannesburg', 'Chennai', 'Taipei', 'Baghdad', 'Santiago', 'Bangalore', 'Hyderabad', 'St Petersburg', 'Philadelphia', 'Lahore', 
+'Kinshasa', 'Miami', 'Ho Chi Minh City', 'Madrid', 'Tianjin', 'Kuala Lumpur', 'Toronto', 'Milan', 'Shenyang', 'Dallas', 'Boston', 'Belo Horizonte', 'Khartoum', 
+'Riyadh', 'Singapore', 'Washington', 'Detroit', 'Barcelona', 'Houston', 'Athens', 'Berlin', 'Sydney', 'Atlanta', 'Guadalajara', 'San Francisco', 'Montreal.', 
+'Monterey', 'Melbourne', 'Ankara', 'Recife', 'Phoenix', 'Durban', 'Porto Alegre', 'Dalian', 'Jeddah', 'Seattle', 'Cape Town', 'San Diego', 'Fortaleza', 
+'Curitiba', 'Rome', 'Naples', 'Minneapolis', 'Tel Aviv', 'Birmingham', 'Frankfurt', 'Lisbon', 'Manchester', 'San Juan', 'Katowice', 'Tashkent', 'Fukuoka', 
+'Baku', 'St. Louis', 'Baltimore', 'Sapporo', 'Tampa', 'Taichung', 'Warsaw', 'Denver', 'Cologne', 'Hamburg', 'Dubai', 'Pretoria', 'Vancouver', 'Beirut', 
+'Budapest', 'Cleveland', 'Pittsburgh', 'Campinas', 'Harare', 'Brasilia', 'Kuwait', 'Munich', 'Portland', 'Brussels', 'Vienna', 'San Jose', 'Damman', 
+'Copenhagen', 'Brisbane', 'Riverside', 'Cincinnati', 'Accra', 'San Bernardino', 'St. Petersburg', 'St. Paul', 'Dusseldorf', 'Kobe', 'Kyoto', 'Yokohama', 
+'Incheon', 'Fort Worth', 'Sumqayit', 'Japan', 'USA', 'Brazil', 'South Korea', 'Mexico', 'Japan', 'Philippines', 'India', 'India', 'Indonesia', 'Nigeria', 
+'India', 'Egypt', 'USA', 'Argentina', 'Brazil', 'Russia', 'China', 'Pakistan', 'France', 'Turkey', 'Japan', 'China', 'USA', 'UK', 'China', 'Germany', 'Iran', 
+'Colombia', 'Peru', 'Thailand', 'South Africa', 'India', 'Taiwan', 'Iraq', 'Chile', 'India', 'India', 'Russia', 'USA', 'Pakistan', 'Congo', 'USA', 'Vietnam', 
+'Spain', 'China', 'Malaysia', 'Canada', 'Italy', 'China', 'USA', 'USA', 'Brazil', 'Sudan', 'Saudi Arabia', 'Singapore', 'USA', 'USA', 'Spain', 'USA', 
+'Greece', 'Germany', 'Australia', 'USA', 'Mexico', 'USA', 'Canada', 'Mexico', 'Australia', 'Turkey', 'Brazil', 'USA', 'South Africa', 'Brazil', 'China', 
+'Saudi Arabia', 'USA', 'South Africa', 'USA', 'Brazil', 'Brazil', 'Italy', 'Italy', 'USA', 'Israel', 'UK', 'Germany', 'Portugal', 'UK', 'Puerto Rico', 'Poland', 
+'Uzbekistan', 'Japan', 'Azerbaijan', 'USA', 'USA', 'Japan', 'USA', 'Taiwan', 'Poland', 'USA', 'Germany', 'Germany', 'UAE', 'South Africa', 'Canada', 'Lebanon', 
+'Hungary', 'USA', 'USA', 'Brazil', 'Zimbabwe', 'Brazil', 'Kuwait', 'Germany', 'USA', 'Belgium', 'Austria', 'USA', 'Saudi Arabia', 'Denmark', 'Australia', 'USA', 
+'USA', 'Ghana'
 ];
 const searchablePlaces = new Set(places);
